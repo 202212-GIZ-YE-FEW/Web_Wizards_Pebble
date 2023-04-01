43208e2f100d6082e63acac8642c731844909626
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-04-01 16:57:56 +0300

    fix(404): add needed serverSideTranslations

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,16 @@
-import * as React from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import Layout from "@/layout/Layout";
 
 export default function NotFoundPage() {
     return <Layout>404</Layout>;
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            // Will be passed to the page component as props.
+        },
+    };
 }
