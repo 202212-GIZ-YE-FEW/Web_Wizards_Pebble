a1e8e0e326867ec7d1dac261a67b69c71430b4d5
Sufyan <sufianb.contact@gmail.com>
2023-04-02 19:56:11 +0300

    feat: the props object is being destructured into separate variables to access them easily
    the props object is being destructured into separate variables icon, alt, width, and height to
    access them easily.

--- src/components/ButtonAuth.jsx
+++ src/components/ButtonAuth.jsx
@@ -5,19 +5,14 @@ import "flatifycss/dist/css/flatify-min.css";
 import styles from "./ButtonAuth.module.css";
 
 const ButtonAuth = (props) => {
-    const { icon, alt, width, height } = props
+    const { icon, alt, width, height } = props;
     return (
         <>
             <Button
                 className={`w-full h-14 px-6 transition ${styles.ButtonAuth}`}
             >
                 <div className='relative flex items-center space-x-4 justify-center'>
-                    <Image
-                        src={icon}
-                        alt={alt}
-                        width={width}
-                        height={height}
-                    />
+                    <Image src={icon} alt={alt} width={width} height={height} />
                     <span className='block w-max font-medium tracking-wide text-gray-900 sm:text-base'>
                         {props.text}
                     </span>
