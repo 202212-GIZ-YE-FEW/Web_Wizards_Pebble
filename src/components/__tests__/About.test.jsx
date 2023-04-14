25b66d324ac1c01bf943f313ad71b870649d6099
a.sulmi24 <a.sulmi24@gmail.com>
2023-04-15 01:02:04 +0200

    fix(fix test): test the event creation
    #32

--- src/components/__tests__/About.test.jsx
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import About from "../About";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<About t={(str) => str} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
