e483a0f79edb966db14c9fb7cdd2ca01fb02c283
a.sulmi24 <a.sulmi24@gmail.com>
2023-04-15 00:13:42 +0200

    test(run snpshot test for event creaion and about): run snpshot test for event creaion and about
    #32

--- src/components/LocationSelector/EventCreation.jsx
+++ src/components/LocationSelector/EventCreation.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
 const EventCreation = () => {
+    const [content, setContent] = useState("");
+
+    function handleContent(e) {
+        setContent(e.target.value);
+    }
+
     return (
         <div className='bg-white p-4'>
             <div className='w-full md:w-2/3 mx-auto bg-white rounded-lg p-4 md:p-8 flex flex-col md:flex-row'>
                 <div className='w-full md:w-1/2 flex flex-col justify-between flex-grow'>
+                    <div className='py-3 mb-4 flex flex-col md:flex-row md:justify-between gap-10'>
+                        <div className='w-full pr-20'>
+                            <h2
+                                className='text-xl font-medium py-1 text-black'
+                                style={{ color: "#1A1A1A", fontWeight: 600 }}
+                            >
+                                Event Description:
+                            </h2>
+                            <p className='my-1 text-gray-500'>
+                                Describe the purpose of your event. Who should
+                                join and what will you do at the event?
+                            </p>
+                            <textarea
+                                value={content}
+                                onChange={handleContent}
+                                style={{
+                                    resize: "none",
+                                    color: "#1A1A1A",
+                                    backgroundColor: "white",
+                                    borderColor: "#1A1A1A",
+                                    borderRadius: "8px",
+                                    borderWidth: "2px",
+                                }}
+                                placeholder='Please write at least 50 characters'
+                                className='w-full h-40 px-3 py-2 rounded border-black shadow focus:border-black focus:outline-none focus:ring-0 focus:ring-black'
+                            />
+                        </div>
+                    </div>
+
                     <div className='mb-3 flex flex-col gap-5 md:flex-row md:justify-between flex-grow'>
                         <div>
                             <h2
