c54d05fda854669a9b787328851c6b655a372252
Sufyan <sufianb.contact@gmail.com>
2023-04-15 06:34:08 +0300

    feat(event view page (single attendees component)): created basecode for Single Attendees
    Created Single Attendees component that shows the Attendees in the event which is going to be used
    with other componetns later

--- src/components/event/SingleAttendees.jsx
+++ src/components/event/SingleAttendees.jsx
@@ -1,9 +1,8 @@
 const SingleAttendees = () => {
-    const firstLetter = name.charAt(0).toUpperCase();
     return (
         <div className='py-2 w-1/3 md:w-1/6 lg:w-1/4'>
             <div className='flex items-center justify-center w-16 h-16 rounded-full bg-black-100 text-white text-md font-bold mb-1'>
-                <span>{firstLetter}</span>
+                <span></span>
             </div>
             <p className='text-sm font-semibold'></p>
         </div>
