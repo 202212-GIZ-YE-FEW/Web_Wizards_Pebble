cc8946f9f43c47ab6335bf9c91ee4640831a2c3d
a.sulmi24 <a.sulmi24@gmail.com>
2023-04-17 06:58:47 +0200

    fix(fix some bugs): fix event creation bugs
    #39

--- src/pages/events/create.jsx
+++ src/pages/events/create.jsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
 import LocationSelectors from "../../components/LocationSelector/LocationSelector";
 import EventCreation from "../../components/LocationSelector/EventCreation";
 
