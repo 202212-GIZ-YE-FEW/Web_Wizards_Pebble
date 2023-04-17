adbfa55f2493bcaec8364063bcc0db342a68ec38
a.sulmi24 <a.sulmi24@gmail.com>
2023-04-17 22:17:59 +0200

    fix(update the event): fix the event ussue
    #39

--- src/components/LocationSelector/EventCreation.jsx
+++ src/components/LocationSelector/EventCreation.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Input from "./LocationInput";
-//import Interests from "../editProfile/Interests";
+import Interests from "../editProfile/Interests";
 const EventCreation = ({ t }) => {
     return (
         <div className='bg-white p-4'>
             <div className='w-full md:w-2/3 mx-auto bg-white rounded-lg p-4 md:p-8 flex flex-col md:flex-row'>
                 <div className='w-full md:w-1/2 flex flex-col justify-between flex-grow'>
-                    <div className='pt-3 flex flex-col'>
-                        {/* <div>
+                    {/* <div className='pt-3 flex flex-col'>
+                        <div>
                             <h2 className='text-black font-medium text-xl py-1'>
                                 {t("EventType.eventTypeTitle")}
                             </h2>
@@ -20,9 +20,9 @@ const EventCreation = ({ t }) => {
                                     button='hidden'
                                 />
                             </div>
-                        </div> */}
+                        </div>
                         <div className='flex justify-between md:flex md:justify-between'></div>
-                    </div>
+                    </div> */}
 
                     <div className='mb-4 flex   flex-col gap-10 py-3 md:flex-row md:justify-between'>
                         <div>
