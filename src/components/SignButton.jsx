a11348965d55fb28b8617a7fef722da9f9bfe546
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-04-17 06:06:41 +0300

    feat(signup): add loading spinner

--- src/components/SignButton.jsx
+++ src/components/SignButton.jsx
@@ -1,9 +1,14 @@
-const SignButton = ({ text }) => {
+import { Loading } from "react-flatifycss";
+
+const SignButton = (props) => {
+    const { text, loading } = props;
+    const isLoading = loading ? "" : "hidden";
     return (
-        <div className='px-3'>
+        <div className='px-3 flex gap-x-4'>
             <button className='w-full lg:w-auto bg-primary-200 focus:outline-none text-white font-semibold rounded-lg text-md px-5 py-3 '>
                 {text}
             </button>
+            <Loading spinner size='lg' theme='orange' className={isLoading} />
         </div>
     );
 };
