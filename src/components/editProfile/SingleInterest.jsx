3c2cfba589edca58e68b910fa940b5a7a6492662
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-04-25 05:50:42 +0300

    refactor(editprofile): responsive deisgn

--- src/components/editProfile/SingleInterest.jsx
+++ src/components/editProfile/SingleInterest.jsx
@@ -8,20 +8,13 @@ const SingleInterest = ({ text, onInterestClick }) => {
         onInterestClick(text);
     };
 
-    const styles = {
-        border: "2px solid black",
-        borderRadius: "0.5rem",
-        boxShadow: "2px 2px #1a1a1a",
-    };
-
     return (
         <div className='w-[33%] flex justify-center lg:py-3 lg:px-3 md:py-2 md:px-2 py-1 px-1 '>
             <Button
                 className={` w-full lg:h-18 h-16 px-1 transition ${
                     isClicked ? "!bg-primary-100" : "bg-white"
-                } `}
+                } !border-2 !rounded-lg !shadow-[2px_2px_#1A1A1A]`}
                 onClick={handleClick}
-                style={styles}
             >
                 <div className='relative flex items-center justify-center '>
                     <p className='block w-max font-semibold text-gray-900 text-xs py-1 '>
