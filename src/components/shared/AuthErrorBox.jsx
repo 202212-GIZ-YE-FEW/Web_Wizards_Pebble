558d74a592f99769b2ac966b4bf14a91b22eaa9d
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-04-26 10:17:40 +0300

    fix(auth): auth box error close button

--- src/components/shared/AuthErrorBox.jsx
+++ src/components/shared/AuthErrorBox.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Alert, AlertCloseButton } from "react-flatifycss";
 
 export default function AuthErrorBox(props) {
     const { errors, t } = props;
     const theme = "orange-light";
+    const [close, setClosed] = useState(true);
 
     return (
-        <Alert show theme={theme}>
-            <AlertCloseButton theme={theme} label='Close the error alert.' />
+        <Alert show={close} theme={theme}>
+            <AlertCloseButton
+                theme={theme}
+                label='Close the error alert.'
+                onClick={() => setClosed(true)}
+            />
             {t(errors)}
         </Alert>
     );
