0f011db4a37e2c93c6a3a604f3c730f4a0fa43e3
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-04-28 02:45:10 +0300

    chore: deps

--- src/components/__tests__/PetIcons.test.jsx
+++ src/components/__tests__/PetIcons.test.jsx
@@ -3,6 +3,13 @@ import renderer from "react-test-renderer";
 import PetIcons from "@/components/editProfile/PetIcons";
 
 it("renders correctly", () => {
-    const tree = renderer.create(<PetIcons t={(str) => str} />).toJSON();
+    const tree = renderer
+        .create(
+            <PetIcons
+                t={(str) => str}
+                user={{ displayName: "", photoURL: "" }}
+            />
+        )
+        .toJSON();
     expect(tree).toMatchSnapshot();
 });
