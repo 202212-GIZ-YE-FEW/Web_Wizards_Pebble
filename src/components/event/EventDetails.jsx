1768d1d8ce066f5bc7cccb4ea560202cd9616b7a
a.sulmi24 <a.sulmi24@gmail.com>
2023-04-30 10:45:18 +0200

    feat(view event): get the event details and fix some css

--- src/components/event/EventDetails.jsx
+++ src/components/event/EventDetails.jsx
@@ -4,25 +4,29 @@ import { MdLocationPin } from "react-icons/md";
 import SingleDetail from "./SingleDetail.jsx";
 
 const EventDetails = (props) => {
-    const eventData = props.eventData;
+    const eventData = props?.eventData;
     return (
         <div className='px-0 xs:px-12 flex xs:flex-col sm:flex-col lg:flex-wrap md:flex-wrap'>
             <SingleDetail
-                text={eventData.location}
+                text={eventData?.location + " ,Yemen"}
+                icon={<MdLocationPin size={30} />}
+            />
+            <SingleDetail
+                text={eventData?.address}
                 icon={<MdLocationPin size={30} />}
             />
 
             <SingleDetail
-                text={eventData.date}
+                text={eventData?.date}
                 icon={<AiFillClockCircle size={30} />}
             />
             <div className='flex -space-x-3 overflow-hidden'>
-                {eventData.attendees.slice(0, 3).map((attende, index) => (
+                {eventData?.attendees?.slice(0, 3).map((attende, index) => (
                     <div
                         key={index}
                         className=' h-8 w-8 rounded-full bg-gray-900 flex items-center justify-center text-white text-xs'
                     >
-                        <span>{attende.charAt(0)}</span>
+                        <span>{attende?.charAt(0)}</span>
                     </div>
                 ))}
                 <div className='px-2'>
@@ -30,10 +34,10 @@ const EventDetails = (props) => {
                 </div>
             </div>
             <SingleDetail
-                text={`Organized by ${eventData.organizer}`}
+                text={`Organized by ${eventData?.organizer}`}
                 icon={
                     <div className='h-8 w-8 rounded-full bg-gray-900 flex items-center justify-center text-white text-xs'>
-                        <span>{eventData.organizer.charAt(0)}</span>
+                        <span>{eventData?.organizer?.charAt(0)}</span>
                     </div>
                 }
             />
