93a4f85858fe04f19cfa3bbf8311807dd8bd27e0
Sufyan <sufianb.contact@gmail.com>
2023-05-03 12:07:46 +0300

    fix(eventcard): fix responsive design issues for tablet screens
    fix some responsive design issues in the EventCard component that were affecting its appearance on
    tablet screens

--- src/components/events/EventCard/EventCard.jsx
+++ src/components/events/EventCard/EventCard.jsx
@@ -10,26 +10,32 @@ import EventsTitle from "./EventsTitle";
 function EventCard({ event, t }) {
     return (
         <div className='py-4'>
-            <article className='border-2 border-gray-900 rounded-lg py-2 px-3'>
+            <article className='border-2 border-gray-900 rounded-lg py-2 px-3 relative'>
                 <div className='flex flex-row justify-between py-1'>
                     <EventDate EventDate={event.date} />
                     <EventAttendees attendees={event.attendees} t={t} />
                 </div>
-                <div className='flex flex-row'>
+                <div className='flex flex-row py-1'>
                     <div>
                         <EventsImage eventImg={event.image} />
                     </div>
-                    <div className='px-2'>
-                        <EventsTitle title={event.title} />
-                        <EventsDescription description={event.description} />
-                    </div>
-                    <div className='flex justify-end ml-auto mt-auto '>
-                        <a
-                            href={`/events/${event.id}`}
-                            className='bg-primary-200 hover:bg-[#fe9437] !text-white font-bold py-2 px-8 rounded-lg'
-                        >
-                            {t("join")}
-                        </a>
+                    <div className='px-2 flex-grow'>
+                        <div className='flex flex-col h-full'>
+                            <div className='flex-grow px-1'>
+                                <EventsTitle title={event.title} />
+                                <EventsDescription
+                                    description={event.description}
+                                />
+                            </div>
+                            <div className='flex justify-end items-end '>
+                                <a
+                                    href={`/events/${event.id}`}
+                                    className='bg-primary-200 hover:bg-[#fe9437] !text-white font-bold py-2 px-9 rounded-lg'
+                                >
+                                    {t("join")}
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </article>
--- src/components/events/EventCard/EventsImage.jsx
+++ src/components/events/EventCard/EventsImage.jsx
@@ -1,8 +1,8 @@
 const EventsImages = ({ eventImg }) => {
     return (
-        <div className='border-2 rounded-lg overflow-hidden h-28 w-48 sm:h-40 sm:w-64'>
+        <div className='border-2 rounded-lg overflow-hidden h-28 w-48 md:h-40 md:w-64  sm:h-36 sm:w-58'>
             <img
-                className='object-fill h-28 w-48 sm:h-40 sm:w-64'
+                className='object-fill h-28 w-48 md:h-40 md:w-64  sm:h-36 sm:w-58'
                 src={eventImg}
                 alt='Card image'
             />
