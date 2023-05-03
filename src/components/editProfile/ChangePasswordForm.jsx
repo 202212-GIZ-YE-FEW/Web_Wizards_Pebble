9c310549e9646095e41d1863143a422b4293c81f
Sufyan <sufianb.contact@gmail.com>
2023-05-03 13:03:49 +0300

    test(fix a failing test): fix a failing test
    fix a failing test

--- src/components/editProfile/ChangePasswordForm.jsx
+++ src/components/editProfile/ChangePasswordForm.jsx
@@ -6,6 +6,7 @@ const ChangePasswordForm = ({ t }) => {
     return (
         <div className='w-11/12 mx-auto rounded-3xl flex flex-col justify-center bg-[#d4f3fa] md:px-10 px-2 py-10'>
             <SecondaryTitle text={t("changePassword")} />
+
             <div className='flex flex-row py-6'>
                 <InputForm
                     placeholder={t("newPassword")}
