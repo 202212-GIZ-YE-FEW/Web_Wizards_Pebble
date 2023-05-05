a48236d3988ee2634b5d74ce6ae978c3783c6be1
Sufyan <sufianb.contact@gmail.com>
2023-05-05 20:04:58 +0300

    feat(joinbutton): adding Leave button
    adds a new functionality to the JoinButton component to allow attendees to leave events

--- src/components/event/JoinButton.jsx
+++ src/components/event/JoinButton.jsx
@@ -6,6 +6,9 @@ import useFirestore from "@/firebase/firestore";
 const JoinButton = ({ eventId }) => {
     const { user } = useAuthContext();
     const [isJoining, setIsJoining] = useState(false);
+    const [isLeaving, setIsLeaving] = useState(false);
+    const [isJoined, setIsJoined] = useState(false);
+
     const [error, setError] = useState();
     const eventsHook = useFirestore("events");
 
@@ -30,16 +33,50 @@ const JoinButton = ({ eventId }) => {
         }
     };
 
+    const handleLeaveClick = async () => {
+        try {
+            setIsLeaving(true);
+            const event = await eventsHook.getDocumentById(eventId);
+            const attendees = event.attendees || [];
+
+            const updatedAttendees = attendees.filter(
+                (attendee) => attendee !== user.displayName
+            );
+            await eventsHook.updateDocumentById(eventId, {
+                attendees: updatedAttendees,
+            });
+            setIsJoined(false);
+
+            setIsLeaving(false);
+        } catch (error) {
+            console.error("Error leaving event", error);
+            setError(error);
+            setIsLeaving(false);
+        }
+    };
+
     return (
-        <div className='py-12 order-3 lg:order-none px-4'>
-            <button
-                onClick={handleJoinClick}
-                disabled={isJoining}
-                className='w-full bg-primary-200 focus:outline-none text-white font-semibold rounded-lg text-md px-32 py-3 '
-            >
-                Join
-            </button>
-        </div>
+        <>
+            <div className='py-12 order-3 lg:order-none px-4'>
+                <button
+                    onClick={handleJoinClick}
+                    disabled={isJoining}
+                    className='w-full bg-primary-200 focus:outline-none text-white font-semibold rounded-lg text-md px-32 py-3 '
+                >
+                    Join
+                </button>
+            </div>
+
+            <div className='py-12 order-3 lg:order-none px-4'>
+                <button
+                    onClick={handleLeaveClick}
+                    disabled={isJoining}
+                    className='w-full bg-primary-200 focus:outline-none text-white font-semibold rounded-lg text-md px-32 py-3 '
+                >
+                    Leave
+                </button>
+            </div>
+        </>
     );
 };
 
