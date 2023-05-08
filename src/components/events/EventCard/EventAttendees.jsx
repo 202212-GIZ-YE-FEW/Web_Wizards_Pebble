0842da697dff83e0cde987394b42c29202f62d24
Sufyan <sufianb.contact@gmail.com>
2023-05-09 01:31:21 +0300

    refactor(event attendees): enhance design of attendee avatars
    enhances the design of the attendee avatars in the EventAttendees component

--- src/components/events/EventCard/EventAttendees.jsx
+++ src/components/events/EventCard/EventAttendees.jsx
@@ -6,7 +6,7 @@ const EventAttendees = ({ attendees, t }) => {
                 <a
                     href='#'
                     key={index}
-                    className='inline-block w-6 h-6 md:h-7 md:w-7 rounded-full ring-2 ring-white bg-gray-900  '
+                    className='inline-block flex justify-center !no-underline  w-6 h-6 md:h-7 md:w-7 rounded-full ring-2 ring-white bg-gray-900  '
                 >
                     {item.charAt(0)}
                 </a>
